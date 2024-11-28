53badd2326b549e7ed137722f364eb71ade57ad9
lyteabovenyte <lyteabovenyte@gmail.com>
2024-11-28 07:46:07 +0330

    tweaking player_player_edge query a little bit and fixing the issues around group by

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/player_player_edges.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/player_player_edges.sql
@@ -8,29 +8,29 @@ WITH deduped AS (
      ),
      aggregated AS (
           SELECT
-           f1.player_id,
-            f1.player_name,
-           f2.player_id,
-           f2.player_name,
-           CASE WHEN f1.team_abbreviation =         f2.team_abbreviation
+           f1.player_id as subject_identifier,
+
+           f2.player_id as object_identifier,
+
+            CASE WHEN f1.team_abbreviation = f2.team_abbreviation
                 THEN 'shares_team'::edge_type
             ELSE 'plays_against'::edge_type
-            END,
+            END as edge_type,
+            f1.player_name as subject_player_name,
+            f2.player_name as object_player_name,
             COUNT(1) AS num_games,
             SUM(f1.pts) AS left_points,
             SUM(f2.pts) as right_points
         FROM filtered f1
-            JOIN filtered f2
-            ON f1.game_id = f2.game_id
-            AND f1.player_name <> f2.player_name
-        WHERE f1.player_id > f2.player_id
+        JOIN filtered f2
+        ON f1.game_id = f2.game_id
+        AND f1.player_name <> f2.player_name
+        WHERE f1.player_id > f2.player_id -- one sided edge
         GROUP BY
-                f1.player_id,
-            f1.player_name,
-           f2.player_id,
-           f2.player_name,
-           CASE WHEN f1.team_abbreviation =         f2.team_abbreviation
+            f1.player_id,
+            f2.player_id,
+           CASE WHEN f1.team_abbreviation = f2.team_abbreviation
                 THEN  'shares_team'::edge_type
             ELSE 'plays_against'::edge_type
-            END
+            END as edge_type
      )
